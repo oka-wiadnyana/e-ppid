cf65aba9cca1933c0a4fda31d565c120d9f7cd0f
oka-wiadnyana <onsdee86@gmail.com>
2022-05-06 20:39:35 +0800

    update email helper

--- app/Helpers/mailer_helper.php
+++ app/Helpers/mailer_helper.php
@@ -13,15 +13,15 @@ function lets_mail($subject = null, $msg = null, $recipient = null)
         //Server settings
         $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
         $mail->isSMTP();                                            //Send using SMTP
-        $mail->Host       = 'mail.ozavo.my.id';                     //Set the SMTP server to send through
+        $mail->Host       = 'mail.onsdeeapp.my.id';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-        $mail->Username   = 'onsdeeapp@ozavo.my.id';                     //SMTP username
+        $mail->Username   = 'okawiadnyana@onsdeeapp.my.id';                     //SMTP username
         $mail->Password   = 'divio19092019';                               //SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
         $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
         //Recipients
-        $mail->setFrom('onsdeeapp@ozavo.my.id', 'E-Pelita');
+        $mail->setFrom('okawiadnyana@onsdeeapp.my.id', 'E-Pelita');
         foreach ($recipient as $r) {
 
             $mail->addAddress($r);     //Add a recipient
@@ -41,7 +41,7 @@ function lets_mail($subject = null, $msg = null, $recipient = null)
         // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
         $mail->send();
-        return 'Email berhasil dikirim';
+        return 'Email berhasil dikirim, cek di kotak masuk atau pada folder spam';
     } catch (Exception $e) {
         return "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
     }
